b58fe4b6932aa8f70d19231bfe0ab3c09c97a1d4
time-river <me@vvl.me>
2018-03-21 21:13:18 +0800

    update reference & MTU discovery which handling that read how many bytes from tun

--- docs/research/tun-read-write.py
+++ docs/research/tun-read-write.py
@@ -8,12 +8,12 @@ prepare:
     # ip tuntap add mode tun tun0
     # ip link set tun0 up
     # ip addr add 10.0.0.2/24 dev tun0
-    # ip route add default via 10.0.0.2 dev tun0 table 100
-    # ip rule add from all lookup 100 
+    # ip route add default via 10.0.0.2 dev tun0 table 100 # ip rule add from all lookup 100 
     # ip rule add from all iif tun0 lookup main
 
 reference:
     feasibility
+        https://github.com/alwaystest/Blog/issues/20
         https://stackoverflow.com/questions/2664955/can-i-make-a-tcp-packet-modifier-using-tun-tap-and-raw-sockets
         https://serverfault.com/questions/671516/what-exactly-happens-to-packets-written-to-a-tun-tap-device
 
@@ -23,10 +23,15 @@ reference:
 
     matter
         https://serverfault.com/questions/411921/how-to-configure-linux-routing-filtering-to-send-packets-out-one-interface-over
+
+      IP packet max size
+        https://serverfault.com/questions/645890/tcpdump-truncates-to-1472-bytes-useful-data-in-udp-packets-during-the-capture
+        https://gist.github.com/nzjrs/8934855
 '''
 
 import select
 import os
+import socket
 import fcntl
 from fcntl import ioctl
 import struct
@@ -34,12 +39,19 @@ import struct
 TUNSETIFF = 0x400454ca
 IFF_TUN   = 0x0001
 IFF_NO_PI = 0x1000
+SIOCGIFMTU = 0x8921
 
-
-def parse_header():
+def mtu_discovery(ifname):
+    s = socket.socket(type=socket.SOCK_DGRAM)
+    ifs = ioctl(s, SIOCGIFMTU, struct.pack("16s16x", ifname))
+    mtu = struct.unpack('<H',ifs[16:18])[0]
+    print("MTU: ", mtu)
+    return mtu
+    
+def read_all_data_from_tun():
     pass
 
-def loop(fd):
+def loop(fd, mtu):
     flag = fcntl.fcntl(fd, fcntl.F_GETFL)
     fcntl.fcntl(fd, fcntl.F_SETFL, flag|os.O_NONBLOCK)
     flag = fcntl.fcntl(fd, fcntl.F_GETFL)
@@ -55,7 +67,7 @@ def loop(fd):
         for fileno, event in events:
             if event & select.EPOLLIN:
                 print("EPOLLIN")
-                data = os.read(fileno, 1024*1024) # too big package may be result error
+                data = os.read(fileno, mtu)
                 print(len(data), data)
                 os.write(fd, data)
             elif event & select.EPOLLOUT:
@@ -64,6 +76,8 @@ def loop(fd):
                 print(len(data), data)
 
 if __name__ == '__main__':
+    ifname = b'tun0'
     ftun = os.open("/dev/net/tun", os.O_RDWR)
-    ioctl(ftun, TUNSETIFF, struct.pack("16sH", b"tun0", IFF_TUN | IFF_NO_PI))
-    loop(ftun)
+    ifs = ioctl(ftun, TUNSETIFF, struct.pack("16sH", ifname, IFF_TUN | IFF_NO_PI))
+    mtu = mtu_discovery(ifname)
+    loop(ftun, mtu)
