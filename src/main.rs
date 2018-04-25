d29a89e7cab479889b0cec238ec12d3a58a7f1f9
time-river <me@vvl.me>
2018-04-25 23:42:47 +0800

    channel with debug lines

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,55 @@ extern crate bitflags;
 extern crate libc;
 extern crate mio;
 extern crate mio_extras;
-extern crate ipc_channel;
 
 mod driver;
 
 use std::thread;
+use std::vec::Vec;
 use std::io::{Read, Write};
-//use std::sync::mpsc;
-use ipc_channel::ipc::channel;
 
-use mio::{Events, Poll, Token, Ready, PollOpt};
+use mio::{Events, Token, Ready, PollOpt};
+use mio_extras::channel::channel;
 
 use driver::{Iface, MioWrapper};
 
 fn main() {
-// epoll
     let iface = Iface::new("tun0").unwrap();
     iface.up();
 
     let mut stream = MioWrapper::new(iface).unwrap();
 
+    let (tx_1, rx_1) = channel();
+    let (tx_2, rx_2) = channel();
+
+    let _ = thread::spawn(move || {
+        let (tx, rx) = (tx_2, rx_1);
+
+        let mut events = Events::with_capacity(1024);
+
+        let poll = mio::Poll::new().unwrap();
+        poll.register(&rx, Token(1), Ready::readable(), PollOpt::level()).unwrap();
+
+        loop {
+
+            let size = poll.poll(&mut events, None).unwrap();
+
+            println!("subthread size={}", size);
+            for event in &events {
+                if event.token() == Token(1) && event.readiness().is_readable() {
+                    let buf: Vec<u8> = rx.try_recv().unwrap();
+                    println!("recv from main, size={}", buf.len());
+                    tx.send(buf).unwrap();
+                }
+            }
+        }
+    });
+    
+    let (tx, rx) = (tx_1, rx_2);
+
     let poll = mio::Poll::new().unwrap();
-    poll.register(&stream, Token(0), Ready::readable()|Ready::writable(), PollOpt::edge()).unwrap();
+    poll.register(&rx, Token(1), Ready::readable(), PollOpt::level()).unwrap();
+    poll.register(&stream, Token(0), Ready::readable(), PollOpt::level()).unwrap();
 
     let mut events = Events::with_capacity(1024);
 
@@ -32,60 +59,20 @@ fn main() {
     loop {
         let size = poll.poll(&mut events, None).unwrap();
 
-        println!("poll return size {}", size);
+        println!("main size={}", size);
         for event in &events {
             i += 1;
-            if event.token() == Token(0) && event.readiness().is_readable() && event.readiness().is_writable() {
+            if event.token() == Token(0) && event.readiness().is_readable() {
 				let mut buf = vec![0u8; stream.get_buffer_size()];
                 let len = stream.read(&mut buf).unwrap();
 				buf.resize(len, 0);
-                println!("receive, {}, events_len={}, event={:?}", i, events.len(), event);
+                println!("receive, {}, event={:?}", i, event);
+                tx.send(buf).unwrap();
+            } else if event.token() == Token(1) && event.readiness().is_readable() {
+                let buf: Vec<u8> = rx.try_recv().unwrap();
+                println!("receive from slave, size={}", buf.len());
 				stream.write(&buf).unwrap();
             }
-            if event.token() == Token(0) && event.readiness().is_writable() {
-                println!("writable");
-				let mut buf = vec![0u8; stream.get_buffer_size()];
-                let len = stream.read(&mut buf);
-                println!("read {:?}\n", len);
-            }
         }
     }
 }
-
-/*
-fn main() {
-    let mut iface = Iface::new("tun0").unwrap();
-    iface.up();
-
-    let (tx_1, rx_1) = channel().unwrap();
-    let (tx_2, rx_2) = channel().unwrap();
-
-    let mut i = 0;
-    thread::spawn(move || {
-        let tx = tx_2;
-        let rx = rx_1;
-
-        loop {
-            let buf: Vec<u8> = rx.recv().unwrap();
-            i += 1;
-            println!("receive from main, size={}, i={}", buf.len(), i);
-            tx.send(buf).unwrap();
-        }
-    });
-
-    let tx = tx_1;
-    let rx = rx_2;
-
-    loop {
-        let mut buf = vec![0u8; iface.get_mtu().unwrap()];
-        let len = iface.read(&mut buf).unwrap();
-        buf.resize(len, 0);
-        println!("send, size={}, i={}", buf.len(), i);
-        tx.send(buf).unwrap();
-        let buffer: Vec<u8> = rx.recv().unwrap();
-        iface.write(&buffer).unwrap();
-        drop(buffer);
-    }
-
-}
-*/
